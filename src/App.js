cb526a490d75c7ea432f330e8c4b4fea2058cdcb
AbdullahWins <noyononline@gmail.com>
2023-01-28 16:54:22 +0600

    fixed colors

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       <div className="px-96 py-48 bg-gray-700">
         <div className="card lg:w-96">
           <h2 className="text-center text-2xl font-bold py-4">
-            Abdullah's login page
+            Sign into your website
           </h2>
           <input
             type="text"
             placeholder="username"
-            className="input input-bordered w-full bg-blue-500 text-gray-700 rounded-none"
+            className="input input-bordered w-full bg-white text-gray-700 rounded-none"
           />
           <input
             type="password"
-            placeholder="Password"
-            className="input input-bordered w-full bg-yellow-400 text-gray-700 rounded-none my-2"
+            placeholder="password"
+            className="input input-bordered w-full bg-white text-gray-700 rounded-none my-2"
           />
-          <button className="btn w-full bg-red-400 border-none text-white rounded-none">
+          <button className="btn w-full bg-gray-400 border-none text-white rounded-none">
             Login
           </button>
         </div>
