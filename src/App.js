835a46c7a36ecc917b077814b670ed59e0676d3b
AbdullahWins <noyononline@gmail.com>
2023-01-28 16:45:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:flex h-screen md:justify-center md:items-center mx-2">
+      <div className="px-96 py-48 bg-gray-700">
+        <div className="card lg:w-96">
+          <h2 className="text-center text-2xl font-bold py-4">
+            Abdullah's login page
+          </h2>
+          <input
+            type="text"
+            placeholder="username"
+            className="input input-bordered w-full bg-blue-500 text-gray-700 rounded-none"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full bg-yellow-400 text-gray-700 rounded-none my-2"
+          />
+          <button className="btn w-full bg-red-400 border-none text-white rounded-none">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
